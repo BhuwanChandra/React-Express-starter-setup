6e0381e4d965dd785098d8f9ecd52f0136a7c560
BhuwanChandra <bhuwan7779@gmail.com>
2020-01-24 19:48:47 +0530

    fixed client side vulnerabilities

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 5000;
 
 app.get('/api/customers',(req, res) => {
+    // sample api to which can be  replaced by your own stuff
     const customers = [
         { id: 1, firstName: "John", lastName: 'Walker'},
         { id: 2, firstName: "Steve", lastName: 'Smith'},
